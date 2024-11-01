e801143634edc0aa60b46d723c498c1b13cbb789
Николай Емельянов <kolaemelanov60@gmail.com>
2024-11-01 14:50:19 +0300

    Code without jsx

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,30 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 
 // Весь JSX-код здесь представляет собой декларативный стиль
 export function App() {
 	const currentYear = new Date().getFullYear();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-			<footer>
-				<p>Year - {currentYear}</p>
-			</footer>
-    </div>
+  return createElement(
+		"div",
+		{className: "App"},
+		createElement(
+			"header",
+			{className: "App-header"},
+			createElement(
+				"img",
+				{ src: logo, className: "App-logo", alt: "logo" }
+			),
+			createElement(
+				"p",
+				null,
+				"Edit ",
+				createElement("code", null, "src/App.js "),
+				"and save to reload."
+			),
+			createElement("a", {className: "App-link", href: "https://reactjs.org", target: "__blank", rel: "noopener noreferrer"},
+				"Learn React"
+			)
+		),
+		createElement("footer",
+			null,
+			createElement("p", null, currentYear)
+		)
   );
 }
